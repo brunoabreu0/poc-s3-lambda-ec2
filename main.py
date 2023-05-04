6135240d0c91889038e6711a7dd054c2e50b9aa2
Bruno Abreu <bruno.abreu@redcloudtechnology.com>
2023-05-04 17:59:31 +0100

    Logging to file

--- main.py
+++ main.py
@@ -1,5 +1,16 @@
+import datetime
 import sys
+import time
+from contextlib import redirect_stdout, redirect_stderr
 
 if __name__ == '__main__':
-    print('EXEC EC2 TASK')
-    print(sys.argv)
+    try:
+        log_timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        log_file_path = f'logs/task-{log_timestamp}.log'
+        log_file_handle = open(log_file_path, 'w')
+        with redirect_stdout(log_file_handle), redirect_stderr(log_file_handle):
+            print('EXEC EC2 TASK')
+            print(sys.argv)
+    finally:
+        if log_file_handle is not None:
+            log_file_handle.close()
