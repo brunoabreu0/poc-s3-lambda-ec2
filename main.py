7da663d288f66760ad9d6495860c6774046c82e8
Bruno Abreu <bruno.abreu@redcloudtechnology.com>
2023-05-04 18:57:19 +0100

    logging

--- main.py
+++ main.py
@@ -1,16 +1,34 @@
 import datetime
 import sys
-import time
+import json
 from contextlib import redirect_stdout, redirect_stderr
 
+class Task:
+    def __init__(self, task_id, args):
+        self._task_id = task_id
+        self._args = args
+
+    def log(self, message):
+        log = {
+            'task-id': self._task_id,
+            'timestamp': datetime.datetime.now().strftime('%Y%m%d%H%M%S'),
+            'log-message': message
+        }
+        print(json.dumps(log, indent=4))
+
+    def exec(self):
+        self.log('EXEC EC2 TASK')
+        self.log(self._args)
+
+
 if __name__ == '__main__':
+    log_file_handle = None
     try:
-        log_timestamp = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        log_file_path = f'logs/task-{log_timestamp}.log'
+        task_id = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
+        log_file_path = f'logs/task-{task_id}.log'
         log_file_handle = open(log_file_path, 'w')
         with redirect_stdout(log_file_handle), redirect_stderr(log_file_handle):
-            print('EXEC EC2 TASK')
-            print(sys.argv)
+            task = Task(task_id, sys.argv)
     finally:
         if log_file_handle is not None:
             log_file_handle.close()
