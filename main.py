9076f2441a48d1919c93e15bbeba0b580c014b51
Bruno Abreu <bruno.abreu@redcloudtechnology.com>
2023-05-04 21:20:20 +0100

    logging

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ if __name__ == '__main__':
     log_file_handle = None
     try:
         task_id = datetime.datetime.now().strftime('%Y%m%d%H%M%S')
-        log_file_path = f'logs/task-{task_id}.log'
+        log_file_path = f'/opt/poc-s3-lambda-ec2/logs/task-{task_id}.log'
         log_file_handle = open(log_file_path, 'w')
         with redirect_stdout(log_file_handle), redirect_stderr(log_file_handle):
             task = Task(task_id, sys.argv)
